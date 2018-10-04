a125678d4e2db89b18115f99d0de4d8a68b08989
MADELINE PETERS <madelinepet@hotmail.com>
2018-10-03 17:54:22 -0700

    Serialization done and login through django rest framework\!

---
+++ web/budget_api/serializers.py
@@ -0,0 +1,39 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password', 'first_name', 'last_name')
+
+    def create(self, validated_data):
+        user = super().create({
+            'username': validated_data['username'],
+            'email': validated_data['email'],
+        })
+
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+# retreive user object, create new user object
+
+# From django REST docs
+# class GroupSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = Group
+#         fields = ('url', 'name')
+
+
+# Custom
+# class BudgetSerializer(serializers.HyperlinkedModelSerializer):
+#     owner = serializers.ReadOnlyField(source='user.username')
+#     user = serializers.HyperlinkedRelatedField(view_name='user_detail', read_only=True)
+
+#     class Meta:
+#         model = BudgetSerializer
+
+
